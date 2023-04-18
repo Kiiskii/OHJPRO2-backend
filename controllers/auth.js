77748b062ec2ed060e69bb9f62bb84ab1e118e5d
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-04-18 14:14:45 +0300

    add controller/auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const { validationResult } = require('express-validator');
+
+const User = require('../models/user');
+
+const db = require('../util/database');
+
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const userDetails = {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }
+
+        const result = await User.save(userDetails)
+
+        res.status(201).json({ message: 'User registered!' })
+        
+    } catch(err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+}
