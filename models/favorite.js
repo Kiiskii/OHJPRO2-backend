075806a07f87c8b64df3a848f415b49ee736dbf3
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-02 12:32:47 +0300

    add remove favorite from db if user toggle favorite status back

--- models/favorite.js
+++ models/favorite.js
@@ -11,16 +11,27 @@ module.exports = class Favorites {
     }
 
     static async save(favorites) {
-        const result = await db.query(
-            'INSERT INTO favorites (userid, favid) VALUES ($1, $2)',
-            [favorites.userid, favorites.favid]
-        );
-        // console.log(`favid: ${favorites.userid}`)
-        // console.log(`favid: ${favorites.favid}`)
-        return result;
+        try {
+            const { rows } = await db.query('SELECT COUNT(*) FROM favorites WHERE favid = $1', [favorites.favid]);
+            const favCount = rows[0].count;
+            
+            if (favCount > 0) {
+                await db.query('DELETE FROM favorites WHERE favid = $1', [favorites.favid]);
+                console.log('Favorite removed from db!');
+            } else {
+                const result = await db.query(
+                    'INSERT INTO favorites (userid, favid) VALUES ($1, $2)',
+                    [favorites.userid, favorites.favid],
+                    console.log('Favorite added to db!')
+                );
+                // console.log(`favid: ${favorites.userid}`)
+                // console.log(`favid: ${favorites.favid}`)
+                return result;
+            }
+         } catch(err) {
+            console.error(err);
+            throw err;
+        }
     }
 
-    static delete(id) {
-        return db.query('DELETE FROM favorites WHERE id = $1', [id]);
-    }
 }
