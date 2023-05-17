78eae08a7551bddc0edc3988c46006c9a9da483c
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-17 15:00:23 +0300

    Fix config settings for render.com

--- src/util/database.js
+++ src/util/database.js
@@ -3,10 +3,11 @@ const { Pool } = require('pg');
 const config = require('../config/config.json');
 
 const pool = new Pool({
-    host: config.host,
     user: config.user,
-    database: config.database,
-    password: config.password
+    password: config.password,
+    host: config.host,
+    port: config.port,
+    database: config.database
 });
 
 pool.query("SELECT 1", (err, results) => {
