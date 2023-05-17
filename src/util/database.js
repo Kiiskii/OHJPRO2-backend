58b0c2ca0e61ff102e6a83323a0af7c01d20629e
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-17 15:41:02 +0300

    Commit right version

--- src/util/database.js
+++ src/util/database.js
@@ -3,11 +3,10 @@ const { Pool } = require('pg');
 const config = require('../config/config.json');
 
 const pool = new Pool({
-    user: config.user,
-    password: config.password,
     host: config.host,
-    port: config.port,
+    user: config.user,
     database: config.database,
+    password: config.password
 });
 
 pool.query("SELECT 1", (err, results) => {
