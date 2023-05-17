8e0a66a409652f22a86666aa1722ce388216b9f2
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-17 15:31:11 +0300

    return previous

--- src/util/database.js
+++ src/util/database.js
@@ -3,10 +3,11 @@ const { Pool } = require('pg');
 const config = require('../config/config.json');
 
 const pool = new Pool({
-    host: config.host,
     user: config.user,
+    password: config.password,
+    host: config.host,
+    port: config.port,
     database: config.database,
-    password: config.password
 });
 
 pool.query("SELECT 1", (err, results) => {
